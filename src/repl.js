3ba5bb8814f38a856b9d4924ea07af51ce4c07dd
Jonathan Claggett <jonathan@claggett.org>
2022-04-21 15:49:47 -0400

    Add ability to persist repl history

--- src/repl.js
+++ src/repl.js
@@ -3,26 +3,50 @@
 
 const os = require('os')
 const process = require('process')
+const path = require('path')
+const fs = require('fs')
+const fsp = require('fs/promises')
 const readline = require('readline')
 
 const chalk = require('chalk')
 
 const rep = require('./rep')
 
-const main = () => {
+const loadHistory = async (historyFile) => {
+  return (await historyFile.readFile())
+    .toString()
+    .split('\n')
+    .reverse()
+}
+
+const saveHistory = async (historyFile, line) => {
+  if (historyFile !== undefined) {
+    await historyFile.write(`${line}\n`)
+  }
+}
+
+const main = async () => {
+  const historyFileName = path.join(process.cwd(), '.lsn_history')
+  const historyFile = fs.existsSync(historyFileName)
+    ? await fsp.open(historyFileName, 'r+')
+    : undefined
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: chalk`{blue _}{cyan :} `,
     terminal: true,
-    historySize: 200
+    historySize: 200,
+    history: await loadHistory(historyFile)
   })
 
-  rl.on('line', (line) => {
+  rl.on('line', async (line) => {
+    await saveHistory(historyFile, line)
     rl.output.write((line === '') ? line : rep.rep(line) + os.EOL)
     rl.prompt()
   }).on('close', () => {
     rl.output.write(os.EOL)
+    historyFile.close()
   }).prompt()
 }
 
