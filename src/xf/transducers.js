ccd1c070e07a1d00dafd7c9a26c6c76f4ba1d05e
Jonathan Claggett <jonathan@claggett.org>
2022-07-20 10:17:35 -0400

    Add demultiplex transducer
    Also (hopefully) improve the readability of the transformer function

--- src/xf/transducers.js
+++ src/xf/transducers.js
@@ -1,49 +1,50 @@
 'use strict'
 
 // Reduced protocol
-const treduced = '@@transducer/reduced'
-const tvalue = '@@transducer/value'
-
 const isReduced = x =>
-  x instanceof Object && x[treduced] === true
+  x instanceof Object && x['@@transducer/reduced'] === true
 
 const unreduced = x =>
   isReduced(x)
-    ? x[tvalue]
+    ? x['@@transducer/value']
     : x
 
 const reduced = x =>
   isReduced(x)
     ? x
-    : { [treduced]: true, [tvalue]: x }
+    : { '@@transducer/reduced': true, '@@transducer/value': x }
 
 // Transformer protocol
-const tinit = '@@transducer/init'
-const tresult = '@@transducer/result'
-const tstep = '@@transducer/step'
+const transformer = (obj) => ({
+  '@@transducer/init': obj.init,
+  '@@transducer/step': obj.step,
+  '@@transducer/result': obj.result
+})
 
-const transformer = (init, step, result) =>
-  ({ [tinit]: init, [tstep]: step, [tresult]: result })
+const init = (t) => t['@@transducer/init']()
+const step = (t, a, v) => t['@@transducer/step'](a, v)
+const result = (t, a) => t['@@transducer/result'](a)
 
 // Transducer functions
 const final = x =>
-  t => transformer(
-    () => t[tinit](),
-    (a, v) => t[tstep](a, v),
-    (a) => t[tresult](unreduced(t[tstep](a, x))))
+  t => transformer({
+    init: () => init(t),
+    step: (a, v) => step(t, a, v),
+    result: (a) => result(t, unreduced(step(t, a, x)))
+  })
 
 const multiplex = (xfs) =>
   t => {
     let ts = xfs.map(xf => xf(t))
-    return transformer(
-      () => t[tinit](),
-      (a, v) => {
+    return transformer({
+      init: () => init(t),
+      step: (a, v) => {
         const a3 = ts.reduce(
           (a, t, i) => {
-            const a2 = t[tstep](a, v)
+            const a2 = step(t, a, v)
             if (isReduced(a2)) {
               ts[i] = null // Remove t from ts
-              return t[tresult](unreduced(a2))
+              return result(t, unreduced(a2))
             } else {
               return a2
             }
@@ -56,8 +57,63 @@ const multiplex = (xfs) =>
           return unreduced(a3)
         }
       },
-      (a) => ts.reduce((a, t) => t[tresult](a), a))
+      result: (a) => ts.reduce((a, t) => result(t, a), a)
+    })
   }
+
+const demultiplexTransformer = (state, t) =>
+  // Warning: state is mutated by the transformer created in this function
+  transformer({
+    init: () => {
+      state.running = true
+      if (state.init == null) {
+        state.init = init(t)
+      }
+      return state.init
+    },
+    step: (a, v) => {
+      state.running = true
+      if (state.reduced == null) {
+        const a2 = step(t, a, v)
+        if (isReduced(a2)) {
+          state.reduced = a2
+        }
+        return a2
+      } else {
+        return state.reduced
+      }
+    },
+    result: (a) => {
+      state.running = true
+      if (state.result == null) {
+        state.refCount -= 1
+        if (state.refCount === 0 || state.reduced != null) {
+          state.result = result(t, a)
+          return state.result
+        } else {
+          return a
+        }
+      } else {
+        return state.result
+      }
+    }
+  })
+
+const demultiplex = (xf) => {
+  // Shared, mutable state across multiple calls to the transducer.
+  // This makes this function not thread safe.
+  let state = { running: true }
+  return t => {
+    if (state.running) {
+      state = { running: false, refCount: 0 }
+    }
+    state.refCount += 1
+    if (state.transformer == null) {
+      state.transformer = demultiplexTransformer(state, xf(t))
+    }
+    return state.transformer
+  }
+}
 
 module.exports = {
   isReduced,
@@ -65,5 +121,6 @@ module.exports = {
   unreduced,
 
   final,
-  multiplex
+  multiplex,
+  demultiplex
 }
