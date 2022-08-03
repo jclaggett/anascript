e003f03a765851348d04eab1bcbcc883f88163fd
Jonathan Claggett <jonathan@claggett.org>
2022-08-02 22:08:56 -0400

    Add support for embedded nets.
    A near complete rewrite. Removed the classes and changed the public
    facing API to use an object to label the various nodes in the net
    (including special embedded nodes).

--- src/xf/net.js
+++ src/xf/net.js
@@ -1,68 +1,3 @@
-class GenericNode {
-  constructor (id) {
-    this.id = id
-    this.inputs = []
-  }
-}
-
-class InputNode extends GenericNode {
-  constructor (id) {
-    super(id)
-    this.type = 'input'
-  }
-}
-
-class InternalNode extends GenericNode {
-  constructor (id, value, inputs) {
-    super(id)
-    this.type = 'node'
-    this.inputs = inputs
-    this.value = value
-  }
-}
-
-class OutputNode extends GenericNode {
-  constructor (id, inputs) {
-    super(id)
-    this.type = 'output'
-    this.inputs = inputs
-  }
-}
-
-const makeNetMapEntry = (netMap, node) => {
-  netMap.nodes[node.id] = {
-    type: node.type,
-    value: node.value,
-    inputs: [],
-    outputs: []
-  }
-
-  if (node.type === 'input') netMap.inputs.push(node.id)
-  if (node.type === 'output') netMap.outputs.push(node.id)
-
-  return node.inputs.reduce(
-    (netMap, subnode) => {
-      if (netMap.nodes[subnode.id] == null) {
-        netMap = makeNetMapEntry(netMap, subnode)
-      }
-
-      netMap.nodes[subnode.id].outputs.push(node.id)
-      netMap.nodes[node.id].inputs.push(subnode.id)
-      return netMap
-    },
-    netMap)
-}
-
-const makeNetMap = (id, inputs) => {
-  const netMap = {
-    id,
-    inputs: [],
-    nodes: {},
-    outputs: []
-  }
-  return inputs.reduce(makeNetMapEntry, netMap)
-}
-
 const walkNetMap = (netMap, rootKey, walkFn) => {
   const childKey = rootKey === 'outputs' ? 'inputs' : 'outputs'
 
@@ -86,26 +21,121 @@ const walkNetMap = (netMap, rootKey, walkFn) => {
     netMap[rootKey].map(id => walked[id]))
 }
 
-const makeNetTransducer = (_netMap) =>
-  x => x
+const makeNetTransducer = (netMap) => {
+  let cachedTransducer
+  const applyCachedTransducer = (xf) => {
+    if (cachedTransducer == null) {
+      // TODO actually make the transducer from netMap
+      // walkNetMap(netMap, 'inputs', () => null)
+      cachedTransducer = x => x
+    }
+    return cachedTransducer(xf)
+  }
 
-const input = (id) =>
-  new InputNode(id, [])
-const node = (id, value, ...inputs) =>
-  new InternalNode(id, value, inputs)
-const output = (id, ...inputs) =>
-  new OutputNode(id, inputs)
-const net = (id, ...inputs) => {
-  const netMap = makeNetMap(id, inputs)
-  makeNetTransducer(netMap)
+  return (...args) =>
+    args.length === 0
+      ? netMap
+      : applyCachedTransducer(...args)
+}
+
+const input = () => ({ type: 'input', inputs: [] })
+const node = (xf, ...inputs) => ({ type: 'node', xf, inputs })
+const output = (...inputs) => ({ type: 'output', inputs })
+const embed = (nxf, inputs) => ({ type: 'embed', nxf, inputs })
+
+const sanitizeNetMap = (netMap) =>
+  netMap
+
+const pushValue = (obj, key, value) => {
+  const values = obj[key] ?? []
+  values.push(value)
+  return obj
+}
+
+const connectNodeDirection = (netMap, a, b, dir) => {
+  if (a instanceof Array) {
+    pushValue(netMap.nodes[a[0]][dir], a[1], b)
+  } else {
+    netMap.nodes[a][dir].push(b)
+  }
   return netMap
 }
 
+const connectNodes = (netMap, a, b) => {
+  netMap = connectNodeDirection(netMap, a, b, 'outputs')
+  netMap = connectNodeDirection(netMap, b, a, 'inputs')
+  return netMap
+}
+
+const initNetMapEntry = (node) => ({
+  ...node,
+  ...(node.type === 'embed'
+    ? { inputs: {}, outputs: {} }
+    : { inputs: [], outputs: [] })
+})
+
+const makeNetMap = (id, netSpec) => {
+  const makeNetMapEntry = (netMap, fullId) => {
+    const id = fullId instanceof Array ? fullId[0] : fullId
+
+    if (netMap.nodes[id] != null) return netMap
+
+    const node = netSpec[id]
+
+    if (node.type === 'input') netMap.inputs.push(id)
+    if (node.type === 'output') netMap.outputs.push(id)
+
+    netMap.nodes[id] = initNetMapEntry(node)
+
+    return (node.type === 'embed'
+      ? Object.entries(node.inputs).map(([inputId, inputs]) =>
+        [[id, inputId], inputs])
+      : [[fullId, node.inputs]])
+      .reduce((netMap, [fullId, inputs]) =>
+        inputs.reduce((netMap, fullSubId) =>
+          connectNodes(makeNetMapEntry(netMap, fullSubId), fullSubId, fullId),
+        netMap),
+      netMap)
+  }
+
+  const netMap = Object
+    .entries(netSpec)
+    .filter(entry => entry[1].type === 'output')
+    .map(entry => entry[0])
+    .reduce(makeNetMapEntry, { id, nodes: {}, inputs: [], outputs: [] })
+
+  return sanitizeNetMap(netMap)
+}
+
+const net = (id, netSpec = {}) => {
+  const netMap = makeNetMap(id, netSpec)
+  return makeNetTransducer(netMap)
+}
+
+const simple = net('simple', {
+  in: input(),
+  n: node(x => x, 'in'),
+  out: output('n')
+})
+
+const demo = {
+  empty: net('emptyNet'),
+  simple: net('simpleNet', {
+    i1: input(),
+    n1: node(x => x, 'i1'),
+    n2: node(x => x, 'i1', 'n1'),
+    e1: embed(simple, { in: ['i1', 'n2'] }),
+    e2: embed(simple, { foo: [['e1', 'out2'], 'n1'] }),
+    o1: output('n2', 'i1', ['e2', 'bar'])
+  })
+}
+
 module.exports = {
+  demo,
   input,
+  makeNetMap,
+  net,
   node,
   output,
-  net,
-  makeNetMap,
   walkNetMap
 }
