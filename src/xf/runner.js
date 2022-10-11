b3740646900a50b863b3447819402c7546080eb7
Jonathan Claggett <jonathan@claggett.org>
2022-10-11 10:49:16 -0400

    Add pipe as a special source/sink
    This allows values to flow back into the top of a net as a source from
    the bottom of the net as a sink.
    
    Will also be useful for the upcoming runner sink.

--- src/xf/runner.js
+++ src/xf/runner.js
@@ -10,7 +10,9 @@ const {
   step,
   take,
   active,
-  passive
+  passive,
+  embed,
+  xfnode
 } = require('.')
 
 // A source is a map of async iterables that emit a sequence of values
@@ -43,8 +45,28 @@ const defaultSource = (id) => {
 
 const defaultSink = (id) => {
   console.warn('Unknown sink:', id)
-  return (x) => console.debug(`DEBUG: sink ${id} received '${x}`)
+  return (x) => console.debug(`DEBUG: sink ${id} received:`, x)
 }
+
+const pipes = {}
+
+const defaultPipe = (name) =>
+  (x) => console.debug(
+    `Unknonwn or closed pipe ${name}. ignoring:`, x)
+
+const setPipe = (name, transformer) => {
+  pipes[name] = (x) =>
+    setImmediate(() => {
+      if (isReduced(step(transformer, undefined, x))) {
+        pipes[name] = undefined
+      }
+    })
+}
+
+const getPipe = (name) =>
+  (x) => ((pipes[name] == null)
+    ? defaultPipe(name)
+    : pipes[name])(x)
 
 const run = async (netMap) => {
   await Promise.all(
@@ -54,9 +76,14 @@ const run = async (netMap) => {
           return
         }
 
-        const name = (value.value?.name ?? id)
-        const source = sources[name] ?? defaultSource(name)
         const transformer = xf(t.count())
+
+        if (value.value?.type === 'pipe') {
+          return setPipe(value.value?.name, transformer)
+        }
+
+        const type = (value.value?.type ?? id)
+        const source = sources[type] ?? defaultSource(type)
 
         for await (const x of source(value.value)) {
           if (isReduced(step(transformer, undefined, x))) {
@@ -69,19 +96,39 @@ const run = async (netMap) => {
         if (value.type !== 'sink') {
           return []
         }
-        const name = value.value?.name ?? id
-        return [t.map(sinks[name] ?? defaultSink(name))]
+
+        const type = value.value?.type ?? id
+        const sink = type === 'pipe'
+          ? getPipe(value.value?.name)
+          : sinks[type] ?? defaultSink(type)
+        return [t.map(sink)]
       }
     }))
 }
 
 const ex1 = net({
   init: source({}),
-  log: sink({}, $.init)
+  time: source({ freq: 500 }),
+  take5: take(5, $.time),
+  user: map(x => x.env.USER, $.init),
+  usertime: map((user, time) => [user, time],
+    $.user,
+    $.take5),
+  log: sink({}, $.usertime),
+  debugtake: map(x => `debugging take 5: ${x}`, $.take5),
+  debug: sink({}, $.debugtake)
+})
+
+const foo = net({
+  init: source({}),
+  time: source({ freq: 0 }),
+  myinit: map(_ => ({ env: { USER: 'nmosher' } }), $.init),
+  ex1: embed(ex1, { init: $.myinit, time: $.time }),
+  debug: sink({}, [$.ex1.log])
 })
 
 const ex2 = net({
-  N: source({ name: 'time', freq: 100 }),
+  N: source({ type: 'time', freq: 100 }),
   t5: take(5, $.N),
   t10: take(10, $.N),
   msg: map((x, y) => `logging ${x} ${y}`, active($.t5), passive($.t10)),
@@ -95,13 +142,37 @@ const ex3 = net({
   t5b: take(5, $.time),
   log: sink({}, [$.t5a, $.t5b])
 })
+
+const ex4 = net({
+  time: source({ freq: 500 }),
+  pipeOut: source({ type: 'pipe', name: 'foo' }),
+
+  time5: take(5, $.time),
+  node1: map((tsnew, tsold) => tsnew - tsold,
+    $.time5,
+    passive([$.time5, $.pipeOut])),
+  node5: take(5, $.node1),
+
+  pipeIn: sink({ type: 'pipe', name: 'foo' }, $.node5),
+  log: sink({}, $.node5)
+})
+
+const ex5 = net({
+  init: source({}),
+
+  n1: map(x => x.env.USER, $.init),
+  n2: xfnode(t.mapIndexed((x, i) => [x, i]), $.init),
+
+  log: sink({}, [$.n1, $.n2])
+})
+
 /*
 const any = (...x) => x
 const all = (...x) => x
 
 const exampleB = net({
   nodes: {
-    N: source({ name: 'time', freq: 100 }),
+    N: source({ type: 'time', freq: 100 }),
     t5: take(5),
     msg: map((x, y) => `logging ${x} ${y}}`),
     log: sink(),
@@ -116,4 +187,4 @@ const exampleB = net({
 })
 */
 
-module.exports = { run, ex1, ex2, ex3, sources, sinks }
+module.exports = { run, foo, ex1, ex2, ex3, ex4, ex5, sources, sinks }
