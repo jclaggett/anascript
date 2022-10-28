0e60765b7bf977e180c18300d1644a95e43c2799
Jonathan Claggett <jonathan@claggett.org>
2022-10-28 12:17:10 -0400

    Clean up ls and ls2 examples

--- examples/ls.js
+++ examples/ls.js
@@ -1,32 +1,38 @@
 const { flatMap } = require('transducist')
-const { $, map, net, sink, source, xfnode } = require('.')
+const {
+  $, map, net, sink, source, xfnode
+} = require('../src/xf')
+
+const makeDirNets = ({ dirnames, padding, useTitles }) =>
+  dirnames.map(dirname =>
+    net({
+      init: source({ type: 'init' }),
+      entry: source({ type: 'dir', path: dirname }),
+
+      title: map(_ => `\n${dirname}:`, $.init),
+      name: map(x => `${padding}${x.name}`, $.entry),
+
+      log: sink({ type: 'log' },
+        [$.name, ...(useTitles ? [$.title] : [])])
+    })
+  )
 
 const ls = net({
   init: source({ type: 'init' }),
 
-  config: map(({ argv }) => ({
-    useTitles: argv.length > 1,
-    dirnames: argv
-  }),
+  config: map(({ argv }) => {
+    const useTitles = argv.length > 1
+    return {
+      useTitles,
+      padding: ' '.repeat(useTitles ? 4 : 0),
+      dirnames: argv
+    }
+  },
   $.init),
 
-  dirNet: xfnode(flatMap(({ dirnames, useTitles }) =>
-    dirnames.map(dirname =>
-      net({
-        init: source({ type: 'init' }),
-        entry: source({ type: 'dir', path: dirname }),
+  dirNets: xfnode(flatMap(makeDirNets), $.config),
 
-        title: map(_ => `\n${dirname}:`, $.init),
-        name: map(entry => `${dirname}/${entry.name}`, $.entry),
-
-        log: sink({ type: 'log' },
-          [$.name, ...(useTitles ? [$.title] : [])])
-      })
-    )
-  ),
-  $.config),
-
-  run: sink({ type: 'run' }, $.dirNet)
+  run: sink({ type: 'run' }, $.dirNets)
 })
 
 module.exports = { ls }
--- examples/ls2.js
+++ examples/ls2.js
@@ -1,14 +1,42 @@
-const { $, map, net, sink, source, xfnode, prolog, identity, after } = require('../src/xf')
+const t = require('transducist')
+const {
+  $, map, net, sink, source, xfnode, prolog, identity, after, passive
+} = require('../src/xf')
+
+const takeWhile = (f, inputs) =>
+  xfnode(t.takeWhile(f), inputs)
+
+const makeDirNet = ([dirname, ...dirnames], { padding, useTitles }) =>
+  net({
+    entries: source({ type: 'dir', path: dirname }),
+    entryNames: map(x => `${padding}${x.name}`, $.entries),
+
+    entryNames2: xfnode(
+      useTitles
+        ? prolog(`\n${dirname}:`)
+        : identity,
+      $.entryNames),
+
+    log: sink({ type: 'log' }, $.entryNames2),
+
+    dirRest: xfnode(after(dirnames), $.entryNames2),
+    dirSink: sink({ type: 'pipe', name: 'dirnames' }, $.dirRest)
+  })
 
 const ls = net({
   // Act 1: Collect configuration and start processing dirnames
   init: source({ type: 'init' }),
 
-  config: map(({ argv }) => ({
-    useTitles: argv.length > 1,
-    dirnames: argv
-  }),
+  config: map(({ argv }) => {
+    const useTitles = argv.length > 1
+    return {
+      useTitles,
+      padding: ' '.repeat(useTitles ? 4 : 0),
+      dirnames: argv
+    }
+  },
   $.init),
+
   configDirnames: map(x => x.dirnames, $.config),
 
   dirSink: sink({ type: 'pipe', name: 'dirnames' }, $.configDirnames),
@@ -16,28 +44,11 @@ const ls = net({
   // Act 2: process each dirname in sequence (using config as needed)
   dirSource: source({ type: 'pipe', name: 'dirnames' }),
 
-  dirNet: map(([dirname, ...dirnames], { useTitles }) =>
-    (dirname == null)
-      ? net({})
-      : net({
-        entries: source({ type: 'dir', path: dirname }),
-        entryNames: map(x => x.name, $.entries),
+  dirnames: takeWhile(dirnames => dirnames.length > 0, $.dirSource),
 
-        entryNames2: xfnode(
-          useTitles
-            ? prolog(`\n${dirname}:`)
-            : identity,
-          $.entryNames),
-
-        log: sink({ type: 'log' }, $.entryNames2),
-
-        dirRest: xfnode(after(dirnames), $.entryNames2),
-        dirSink: sink({ type: 'pipe', name: 'dirnames' }, $.dirRest)
-      }),
-  $.dirSource, $.config),
+  dirNet: map(makeDirNet, $.dirnames, passive($.config)),
 
   run: sink({ type: 'run' }, $.dirNet)
-  // debug: sink({ type: 'debug' }, $.configDirnames)
 })
 
 module.exports = { ls }
