16acc2a2e5f7580f409c5c5ecb45ad1ac379f7ca
Jonathan Claggett <jonathan@claggett.org>
2023-04-22 10:25:43 -0500

    WIP on runner rewrite

--- src/xf/runner.js
+++ src/xf/runner.js
@@ -2,10 +2,11 @@
 // transducer graphs and to provide runners that 'transduce' values from
 // sources into sinks via a given graph.
 import { opendir } from 'fs/promises'
+import EventHandler from 'node:events'
 
 import t from 'transducist'
 
-import { isReduced, INIT, STEP, RESULT, unreduced } from './reducing.js'
+import { isReduced, INIT, STEP, RESULT, unreduced, ezducer } from './index.js'
 import { composeGraph } from './xfgraph.js'
 
 const defaultSource = (id) => {
@@ -94,31 +95,163 @@ const runGraph = async (g, parentEnv) => {
   }))
 }
 
+const sinks = {
+  pipe: (args, context) => {
+    const id = args[1]
+    const eventName = `pipe_${id}`
+    context.pipes[args[1]] = (context.pipes[id] ?? 0) + 1
+    // return a transducer
+    return ezducer(() => ({
+      step: (v) => {
+        setImmediate(() => context.ee.emit(eventName, v))
+      },
+
+      result: () => {
+        context.pipes[id] -= 1
+      }
+    }))
+  },
+
+  out: (_args, _context) =>
+    ezducer(() => ({
+      step: (v) => console.log(v)
+    }))
+}
+
+const sources2 = {
+  default: (type, _args, context) => {
+    console.debug(`DEBUG: unknown source ${type} received`)
+    const name = `default-${type}`
+    return {
+      name,
+      start: () => {
+        context.ee.removeAllListeners(name)
+      }
+    }
+  }
+}
+
+const context = {
+  sinks,
+  sources: sources2,
+  pipes: {},
+
+  ee: new EventHandler(),
+  resultFns: new Map()
+}
+
+context.ee.on('removeListener', (name, listener) => {
+  if (context.resultFns.has(listener)) {
+    const accumulatedValue = context.resultFns.get(listener)()
+    console.dir({ event: 'removeListener', name, listener, accumulatedValue })
+    context.resultFns.delete(listener)
+  }
+})
+
+const addListener = (ee, resultFns, name, reducer) => {
+  // 1. init
+  let accumulatedValue = reducer[INIT]()
+
+  // 2. step
+  const step = (data) => {
+    accumulatedValue = reducer[STEP](accumulatedValue, data)
+    if (isReduced(accumulatedValue)) {
+      accumulatedValue = unreduced(accumulatedValue)
+      ee.off(name, step) // Stop once the reducer is done
+    }
+  }
+
+  // 3. result
+  const result = () =>
+    reducer[RESULT](accumulatedValue)
+
+  // associate result to step
+  resultFns.set(step, result)
+  ee.on(name, step)
+}
+
+const runGraph2 = async (g, context) => {
+  const sourceSpecs = composeGraph(g, {
+    leafFn: (_path, value) => {
+      if (!isSink(value)) {
+        return []
+      }
+      if (context.sinks[value[1]] == null) {
+        console.warn(`Unknown sink type specified: ${value[1]}`)
+        return []
+      }
+      return [context.sinks[value[1]](value.slice(2), context)]
+    },
+
+    rootFn: async (_path, value, xf) => {
+      if (!isSource(value)) {
+        return
+      }
+      if (context.sources[value[1]] == null) {
+        console.warn(`Unknown source type specified: ${value[1]}`)
+        return
+      }
+
+      return {
+        ...context.sources[value[1]](value.slice(2), context),
+        reducer: xf(t.count())
+      }
+    }
+  })
+
+  // add each source to context.ee and context.resultFns
+  sourceSpecs.map(({ name, reducer }) =>
+    addListener(context.ee, context.resultFns, name, reducer))
+
+  // start each source
+  sourceSpecs.map(({ start }) => start())
+}
+
+/*
+      const type = value[1]
+      const reducer = xf(t.count()) // Convert the transducer into a reducer here!
+      addListener(context.ee, context.resultFns, name, reducer)
+
+      let accumulatedValue = reducer[INIT]()
+      for await (const x of source(value[2])) {
+        accumulatedValue = reducer[STEP](accumulatedValue, x)
+        if (isReduced(accumulatedValue)) {
+          accumulatedValue = unreduced(accumulatedValue)
+          break // Stop once the reducer is done
+        }
+      }
+
+// Always call result when source is done or when reducer is reduced.
+      return reducer[RESULT](accumulatedValue)
+      */
+
 // run a graph defining argv as following parameters after the graph
+const sources = (argv) => ({
+  init: async function * () {
+    yield { env: process.env, argv }
+  },
+
+  time: async function * ({ freq }) {
+    while (true) {
+      yield Date.now()
+      await new Promise(resolve => setTimeout(resolve, freq))
+    }
+  },
+
+  dir: async function * ({ path }) {
+    const dir = await opendir(path)
+    for await (const dirent of dir) {
+      yield dirent
+    }
+  }
+})
+
 export const run = async (g, ...argv) => {
   const shared = { promises: [] }
   runGraph(g, {
     // this is the root env
     // Each source is an async iterable that emits a sequence of values.
-    sources: {
-      init: async function * () {
-        yield { env: process.env, argv }
-      },
-
-      time: async function * ({ freq }) {
-        while (true) {
-          yield Date.now()
-          await new Promise(resolve => setTimeout(resolve, freq))
-        }
-      },
-
-      dir: async function * ({ path }) {
-        const dir = await opendir(path)
-        for await (const dirent of dir) {
-          yield dirent
-        }
-      }
-    },
+    sources: sources(argv),
 
     // Each sink is a function that performs side effects on its given argument.
     // The return value of a sink is ignored.
@@ -136,6 +269,7 @@ export const run = async (g, ...argv) => {
   const pending = {}
   const pendingPromise = Promise.resolve(pending)
 
+  console.dir(shared)
   while (shared.promises.length > 0) {
     // await for at least one promise to settle
     await Promise.race(shared.promises)
@@ -146,6 +280,7 @@ export const run = async (g, ...argv) => {
       }
     }
     shared.promises = pendingPromises
+    console.dir(shared)
   }
 }
 
@@ -155,3 +290,8 @@ export const sink = (...value) => ['sink', ...value]
 
 const isSource = (x) => Array.isArray(x) && x[0] === 'source'
 const isSink = (x) => Array.isArray(x) && x[0] === 'sink'
+
+// Tasks:
+// 1. convert pipes into async generators.
+// 2. add sink counting for pipes.
+// 3. close pipes when no last sink closes.
